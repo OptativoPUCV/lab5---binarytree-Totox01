99b9abea0c4736a30d606a13de2e8d98f1492013
Replit user <>
2023-09-14 02:56:20 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -37,9 +37,16 @@ TreeNode * createTreeNode(void* key, void * value) {
 }
 
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
-
-    //new->lower_than = lower_than;
+  TreeMap *map = (TreeMap *) malloc(sizeof(TreeMap));
+  if (map == NULL){
+    printf("Error al reservar memoria para el nuevo mapa");
     return NULL;
+  }
+  map->root = NULL;
+  map->current = NULL;
+  map->lower_than = lower_than;
+  //new->lower_than = lower_than;
+  return map;
 }
 
 
